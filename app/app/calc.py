851bae35ed6188cb5dcc25b17e62088e2bb057b4
Benjamin chang <ben.s.chang@gmail.com>
2023-11-26 15:49:47 -0500

    Add postgres db service to docker compose yml file and configure app services for db servics Create a test case using TDD practice

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Fibonacci series 
+"""
+
+def fibonacci_series(number) :    
+    # base case 
+    if number <= 2 : 
+        return number
+    else :
+        return fibonacci_series(number-1) + fibonacci_series(number-2)    
+    
+if __name__ == "__main__" : 
+   print(fibonacci_series(10)) 
+   
---
+++ app/app/test.py
@@ -0,0 +1,15 @@
+"""
+Sample tests 
+"""
+
+from unittest.mock import seal
+from django.test import SimpleTestCase
+from app import calc 
+
+class CalcTests(SimpleTestCase) : 
+    
+    def test_fibonacci(self) : 
+
+        result = calc.fibonacci_series(10)
+        
+        self.assertEqual(result, 89)
