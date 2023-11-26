53830e663ff6f50c3838fa810f457c6d9adcfe2d
Benjamin chang <ben.s.chang@gmail.com>
2023-11-26 18:13:28 -0500

    Implement wait_for_db logic in handler function Apply # noqa to unused import to suppress warnings from Lint Remove trailing spaces, one space before :, adding additional line between classes and imports, etc

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,14 +1,15 @@
 """
-Fibonacci series 
+Fibonacci series
 """
 
-def fibonacci_series(number) :    
-    # base case 
-    if number <= 2 : 
+
+def fibonacci_series(number):
+    # base case
+    if number <= 2:
         return number
-    else :
-        return fibonacci_series(number-1) + fibonacci_series(number-2)    
-    
-if __name__ == "__main__" : 
-   print(fibonacci_series(10)) 
-   
+    else:
+        return fibonacci_series(number-1) + fibonacci_series(number-2)
+
+
+if __name__ == "__main__":
+    print(fibonacci_series(10))
--- app/app/test.py
+++ app/app/test.py
@@ -1,15 +1,15 @@
 """
-Sample tests 
+Sample tests
 """
 
-from unittest.mock import seal
 from django.test import SimpleTestCase
-from app import calc 
+from app import calc
 
-class CalcTests(SimpleTestCase) : 
-    
-    def test_fibonacci(self) : 
+
+class CalcTests(SimpleTestCase):
+
+    def test_fibonacci(self):
 
         result = calc.fibonacci_series(10)
-        
+
         self.assertEqual(result, 89)
